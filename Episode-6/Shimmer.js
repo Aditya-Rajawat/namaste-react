1c68b5be15fb62ff3b60d5679724ad1a43902857
Aditya Rajawat <113927069+Aditya-Rajawat@users.noreply.github.com>
2023-09-14 13:02:55 +0530

    Shimmer.js

---
+++ Episode-6/Shimmer.js
@@ -0,0 +1,92 @@
+const Shimmer = () => {
+  return (
+    <>
+      <div className="shimmer-btn"></div>
+      <div className="shimmer-container">
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+        <div className="shimmer-card">
+          <div className="image"></div>
+          <div className="text-info">
+            <div className="text"></div>
+            <div className="text1"></div>
+            <div className="text2"></div>
+            <div className="text3"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Shimmer;
